9421a0c92f02a3da858e73a352a8cdca22851dd2
Marina Belova <salirsky@mail.ru>
2022-04-11 19:40:19 +0300

    Delete waste comments

--- script.js
+++ script.js
@@ -3,15 +3,11 @@
 const book = document.querySelectorAll(".book");
 const adv = document.querySelector(".adv");
 const body = document.querySelector("body");
-//const h2 = document.querySelectorAll("h2");
 const a = document.querySelectorAll("a");
-
 const ul = document.querySelectorAll("ul");
 const li6 = ul[2].querySelectorAll("li");
 const newLi = document.createElement("li");
-
 const li2 = ul[0].querySelectorAll("li");
-
 const li5 = ul[5].querySelectorAll("li");
 
 // 1 - восстановить порядок книг
@@ -25,12 +21,6 @@ body.style.backgroundImage = "url(/image/you-dont-know-js.jpg)";
 
 // 3 -Исправить заголовок в книге 3( Получится - "Книга 3. this и Прототипы Объектов")
 
-//h2[4].replaceWith("Книга 3. this и Прототипы Объектов");
-//const newH2 = document.createElement("h2");
-
-//newH2.textContent = "Книга 3. this и Прототипы Объектов"
-
-//h2[4].textContent = "Книга 3. this и Прототипы Объектов";
 a[4].textContent = "Книга 3. this и Прототипы Объектов";
 
 // 4 - удалить рекламу со страницы
@@ -44,8 +34,7 @@ li2[3].after(li2[8]);
 li2[3].after(li2[6]);
 
 //5.2 - книга 5
-
-li5[1].after(li5[9]); //  после строки 1 поставить строку 9
+li5[1].after(li5[9]);
 li5[2].after(li5[3]);
 li5[4].after(li5[2]);
 li5[7].after(li5[5]);
@@ -54,15 +43,3 @@ li5[7].after(li5[5]);
 
 newLi.textContent = "Глава 8: За пределами ES6";
 li6[8].append(newLi);
-//li6[8].after("Глава 8: За пределами ES6"); // Поставить текст после главы 8
-//li6[8].innerHTML = "<li>Глава 8: За пределами ES6</li>"; // Поставить текст после главы 8
-
-// ----
-
-// console.log(body);
-
-// console.log(a[4]);
-
-// console.log(ul);
-
-console.log(li5);
